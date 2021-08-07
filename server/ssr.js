b69161efd5578998a4640fe950efaa8a4a38949b
Diablohu <Diablohu@users.noreply.github.com>
2021-08-08 07:33:27 +0800

    Build 8/8/2021, 7:33:27 AM (Local time)

--- server/ssr.js
+++ server/ssr.js
@@ -10905,6 +10905,20 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 const videos = [{
   name: {
+    zh: '新闻: SU5 画质热修更新对比'
+  },
+  des: {
+    zh: '为解决系统更新第5弹带来的各种画质问题，ASOBO 加班加点为《微软模拟飞行》带来了一个全新补丁。接下来我们来看看画质相关修正的改进效果。'
+  },
+  thumbnail: 'thumbnails/db269d1c0763281ac2ca99b9dd53b63c.jpg',
+  link: {
+    youtube: 'https://youtu.be/7NzwvnVcWLo',
+    bilibili: 'https://www.bilibili.com/video/BV13h411B7u9'
+  },
+  tag: ['news'],
+  release: new Date('2021-08-08T12:00:00+0800').valueOf()
+}, {
+  name: {
     zh: '新闻: SU5 补救计划'
   },
   des: {
