f72dc391067fcc37bee9b5d4e971869a21d926a8
Diablohu <Diablohu@users.noreply.github.com>
2021-12-15 22:25:52 +0800

    Build 2021/12/15 下午10:25:51 (Local time)

--- server/ssr.js
+++ server/ssr.js
@@ -60210,7 +60210,7 @@ const videos = [{
     youtube: 'https://youtu.be/57Z9TXEL9sE',
     bilibili: 'https://www.bilibili.com/video/BV1uq4y1B7ub'
   },
-  tag: ['flight'],
+  tag: ['flight', 'tour'],
   release: new Date('2021-12-15T11:10:00+0800').valueOf()
 }, {
   name: {
